a8f707e898374866842be71442d760ee3a478438
정지원 <poiu8944@gmail.com>
2020-11-09 02:53:37 +0900

    manufacture station data & 특정 시간(10초) 이내로 호출할 시 기존 데이터 반환  - create Station class

--- server/api/src/main/java/tashu/api/controller/StationController.java
+++ server/api/src/main/java/tashu/api/controller/StationController.java
@@ -1,7 +1,6 @@
 package tashu.api.controller;
 
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.springframework.web.bind.annotation.*;
 import tashu.api.service.StationService;
 
@@ -19,25 +18,25 @@ public class StationController {
         System.out.println("@@ /station call");
         try{
             stationDataStr = stationService.getStationData();
-//            JSONParser parser = new JSONParser();
-//            stationInfo = (JSONObject) parser.parse(stationDataStr);
         }catch (Exception e){
             e.printStackTrace();
         }
         return stationDataStr;
     }
 
-//
-//    // (id는 대여소 번호)
-//    // TODO: POST, /station/{id}            id에 해당하는 대여소 정보
-//    @ResponseBody
-//    @PostMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
-//    public JSONObject stationId(@PathVariable int id){
-//        RestTemplate restTemplate = new RestTemplate();
-//        return new JSONObject();
-//    }
+/*
+ * client에서 데이터를 가지고 있는다.
+    //    // (id는 대여소 번호)
+    //    // POST, /station/{id}            id에 해당하는 대여소 정보
+    //    @ResponseBody
+    //    @PostMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+    //    public JSONObject stationId(@PathVariable int id){
+    //        RestTemplate restTemplate = new RestTemplate();
+    //        return new JSONObject();
+    //    }
 
-        // TODO: POST, /station/{id}/location   id에 해당하는 대여소 위치
-    // TODO: POST, /station/{id}/bikes      id에 해당하는 대여소의 자전거 대여 현황
+    // POST, /station/{id}/location   id에 해당하는 대여소 위치
+    // POST, /station/{id}/bikes      id에 해당하는 대여소의 자전거 대여 현황
+*/
 
 }
--- server/api/src/main/java/tashu/api/service/StationService.java
+++ server/api/src/main/java/tashu/api/service/StationService.java
@@ -4,29 +4,73 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 
 import static tashu.api.reoisitory.globalConfig.TASHU_STATION_URL;
 
 @Service
 public class StationService {
+    /**
+     * server에서 데이터를 전송할 땐 client에서 필요한 모든 정보를 보낸다.
+     * client에서는 정보를 저장하고 있다가 필요할 때 바로 보여준다.
+     */
 
+    private static Station station = new Station();
+    private static LocalDateTime _last_update = null;
+    private long _update_gap = 10;
+
+    /**
+     * station_info
+     * {"markers":[
+     *      {
+     *          "dong":"도룡동",
+     *          "cntPrtRack":"0", -- remove
+     *          "percent":"21",
+     *          "name":"무역전시관입구(택시승강장)",
+     *          "kiosk_no":"1",
+     *          "imgFile":"\/html\/images\/station\/9D208000000001.jpg", -- remove
+     *          "gu":"유성구",
+     *          "lng":"127.386131",
+     *          "cntRentable":"3",
+     *          "lat":"36.374580",
+     *          "cntLockOff":"7",
+     *          "cntRackTotal":"14"
+     *       },...
+     * ]}
+     */
     @PostConstruct
-    public String getStationData() throws Exception {
-        System.setProperty("jsse.enableSNIExtension", "false") ;
+    public String getStationData() throws Exception{
+        /*
+          첫 업데이트이거나 마지막 업데이트 이후로 sec초 이상 지났으면 갱신한다.
+         */
+        LocalDateTime nowTime = LocalDateTime.now();
+        if(_last_update == null || _last_update.isBefore(nowTime.minusSeconds(_update_gap))){
+            _updateStationData();
+            _last_update = nowTime;
+        }else {
+            System.out.println("updated: "+_last_update);
+            System.out.println("now: "+nowTime.toString());
+        }
+        return station.toString();
+    }
+
+    private void _updateStationData() throws Exception {
+        System.out.println("_updateStationData call @@");
+        System.setProperty("jsse.enableSNIExtension", "false");
 
         Connection conn = Jsoup.connect(TASHU_STATION_URL);
         conn.sslSocketFactory(new CustomSSLSocketFactory());
 
-        Document doc = null;
-        String docBody = "";
-        try{
+        Document doc;
+        String docBody;
+        try {
             doc = conn.get();
             docBody = doc.body().text();
-        }catch(Exception e){
+            station.setData(docBody);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return docBody;
     }
+
 }
