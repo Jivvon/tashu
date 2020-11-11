d39289b30f8a08c34677e9f95a1d8717dbefdddf
kimtaeil <taeil@Taeilui-MacBookPro.local>
2020-11-11 20:25:33 +0900

    현재 위치 수정 중

--- App.js
+++ App.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import NaverMapView, {Circle, Marker, Path, Polyline, Polygon} from 'react-native-nmap';
+import Geolocation from "react-native-geolocation-service"
+import { Platform } from 'react-native';
+
+async function requestPermission(){
+    try{
+        if (Platform.OS === "ios"){
+            return await Geolocation.requestAuthorization("always");
+        }
+    } catch(e)
+{
+    console.log(e);
+}
+}
 
 const App: () => React$Node = () => {
+
+    const [location, setLocation] = useState();
+    useEffect(() => {
+        
+            
+                Geolocation.getCurrentPosition(
+                    pos => {
+                        const{latitude, longitude} = pos.coords;
+                        console.log(pos);
+                        setLocation({
+                            latitude,
+                            longitude,
+                        });
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                );
+    }, []);
     const P0 = {latitude: 37.564362, longitude: 126.977011};
-    const P1 = {latitude: 37.565051, longitude: 126.978567};
+    const P1 = {latitude: 37.564362, longitude: 126.977011};
     const P2 = {latitude: 37.565383, longitude: 126.976292};
+    
 
     return <NaverMapView style={{width: '100%', height: '100%'}}
                          showsMyLocationButton={true}
-                         center={{...P0, zoom: 16}}
+                         center={{...location, zoom: 16}}
                          onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
                          onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
                          onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
-        <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')}/>
-        <Marker coordinate={P1} pinColor="blue" onClick={() => console.warn('onClick! p1')}/>
-        <Marker coordinate={P2} pinColor="red" onClick={() => console.warn('onClick! p2')}/>
-        <Path coordinates={[P0, P1]} onClick={() => console.warn('onClick! path')} width={10}/>
-        <Polyline coordinates={[P1, P2]} onClick={() => console.warn('onClick! polyline')}/>
-        <Circle coordinate={P0} color={"rgba(255,0,0,0.3)"} radius={200} onClick={() => console.warn('onClick! circle')}/>
-        <Polygon coordinates={[P0, P1, P2]} color={`rgba(0, 0, 0, 0.5)`} onClick={() => console.warn('onClick! polygon')}/>
+        <Marker coordinate={location} onClick={() => console.warn('onClick! p0')}/>
     </NaverMapView>
 }
 
