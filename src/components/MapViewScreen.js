bee9a36251323b2939c3e81dc0c08782ee00b5e9
Jiwon Jeong <poiu8944@gmail.com>
2020-12-10 16:38:36 +0900

    출발도착 초기화 및 중복제거

--- src/components/MapViewScreen.js
+++ src/components/MapViewScreen.js
@@ -14,6 +14,7 @@ const MapViewScreen = ({ navigation }) => {
         if (station) await setModalProps(station);
         await setModalVisible(!modalVisible);
     }
+    const CNU_center = {latitude: 36.362178, longitude: 127.344742}
     
     useEffect(() => {
         requestLocationPermission();
@@ -21,13 +22,33 @@ const MapViewScreen = ({ navigation }) => {
     useEffect(() => {
         // TODO: url 출발 도착으로 수정
         if (start && destination) {
+            if (start == destination) {
+                setDestination();
+                return;
+            }
             console.log(`출발 : ${start.name}, 도착: ${destination.name}`)
             const url = "nmap://route/bicycle?slat=37.4640070&slng=126.9522394&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=37.5209436&dlng=127.1230074&dname=%EC%98%AC%EB%A6%BC%ED%94%BD%EA%B3%B5%EC%9B%90&appname=org.reactjs.native.example.SE-term";
+            setStart();
+            setDestination();
             openNaverMapApp(url);
         }
     }, [start, destination])
 
-    const CNU_center = {latitude: 36.362178, longitude: 127.344742}
+    const stationMarkers = stations.map((station) => {
+        station.location = {
+            latitude: +station.location["latitude"],
+            longitude: +station.location["longitude"]
+        };
+
+        return (
+            <TouchableOpacity>
+                <Marker key={station.kiosk_no} coordinate={station.location} pinColor="blue"
+                    onClick={(e) => {
+                        toggleModal(station);
+                    }}></Marker>
+            </TouchableOpacity>
+        )
+    })
 
     return <>
         <ModalView modalVisible={modalVisible}
@@ -43,21 +64,7 @@ const MapViewScreen = ({ navigation }) => {
                       onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
                       onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}
                       useTextureView>
-            {stations.map((station) => {
-                station.location = {
-                    latitude: +station.location["latitude"],
-                    longitude: +station.location["longitude"]
-                };
-
-                return (
-                    <TouchableOpacity>
-                        <Marker key={station.kiosk_no} coordinate={station.location} pinColor="blue"
-                            onClick={(e) => {
-                                toggleModal(station);
-                            }}></Marker>
-                    </TouchableOpacity>
-                )
-            })}
+            {stationMarkers}
             
             <Marker coordinate={CNU_center} pinColor="red" onClick={() => console.warn('CNU center')}/>
         </NaverMapView>
