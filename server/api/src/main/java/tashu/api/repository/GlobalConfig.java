a85396a22702bf1f21263195305ea29e160c5c37
Jiwon Jeong <poiu8944@gmail.com>
2020-12-22 11:10:21 +0900

    fix: server .gitignore

---
+++ server/api/src/main/java/tashu/api/repository/GlobalConfig.java
@@ -0,0 +1,5 @@
+package tashu.api.repository;
+
+public class GlobalConfig {
+  public static final String TASHU_STATION_URL = "[url]";
+}
---
+++ server/api/src/main/java/tashu/api/service/CustomSSLSocketFactory.java
@@ -0,0 +1,40 @@
+package tashu.api.service;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class CustomSSLSocketFactory {
+    public CustomSSLSocketFactory() {
+    }
+
+    public static void setSSL() throws NoSuchAlgorithmException, KeyManagementException {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init((KeyManager[])null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+    }
+}
